4129cb3af4e125bc2fdda2ff75d4d9d294a0d164
Daz <daz.lee1987@gmaillcom>
2014-05-13 22:08:17 +0800

    add http post

---
+++ src/com/amk2/musicrunner/running/StoreRunningEvent.java
@@ -0,0 +1,28 @@
+package com.amk2.musicrunner.running;
+
+import android.os.AsyncTask;
+
+import com.amk2.musicrunner.start.NetworkAccess;
+
+import java.io.IOException;
+
+/**
+ * Created by ktlee on 5/12/14.
+ */
+public class StoreRunningEvent extends AsyncTask<String, Void, Void> {
+    NetworkAccess na;
+
+    public StoreRunningEvent () {
+        na = new NetworkAccess();
+    }
+    @Override
+    protected Void doInBackground(String... voids) {
+        String mockJson = "{hello:'world'}";
+        try {
+            na.HttpPost(NetworkAccess.storeRunningEventUrlString, mockJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/com/amk2/musicrunner/start/NetworkAccess.java
+++ src/com/amk2/musicrunner/start/NetworkAccess.java
@@ -2,6 +2,13 @@ package com.amk2.musicrunner.start;
 
 import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -14,6 +21,7 @@ public class NetworkAccess {
     public static final String baseWeatherUrlString = "http://ec2-54-187-202-50.us-west-2.compute.amazonaws.com:8080/weatherJSON";//TODO need to append city query behind
     public static final String baseWeatherWeekUrlString = "http://ec2-54-187-202-50.us-west-2.compute.amazonaws.com:8080/weatherWeekJSON";//TODO need to append city query behind
     public static final String baseWeather24HoursUrlString = "http://ec2-54-187-202-50.us-west-2.compute.amazonaws.com:8080/weather24HoursJSON";//TODO need to append city query behind
+    public static final String storeRunningEventUrlString = "http://ec2-54-187-202-50.us-west-2.compute.amazonaws.com:8080/store?type=event";//TODO need to append city query behind
     //make http request to download data from server
     public InputStream downloadUrl(String urlString) throws IOException {
         URL url = new URL(urlString);
@@ -27,5 +35,20 @@ public class NetworkAccess {
         conn.connect();
         InputStream stream = conn.getInputStream();
         return stream;
+    }
+
+    public InputStream HttpPost (String urlString, String data) throws IOException{
+        InputStream inputStream = null;
+        HttpClient httpClient = new DefaultHttpClient();
+
+        HttpPost httpPost = new HttpPost(urlString);
+
+        httpPost.setEntity(new StringEntity(data));
+        httpPost.setHeader("Content-Type", "Application/json");
+
+        HttpResponse httpResponse = httpClient.execute(httpPost);
+        inputStream = httpResponse.getEntity().getContent();
+
+        return inputStream;
     }
 }
