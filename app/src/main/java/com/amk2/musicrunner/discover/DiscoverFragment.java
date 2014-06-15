8472afd5e0b658e0ee72976dc770d4699b7df96b
TengChou Yang <threefure@gmail.com>
2014-06-14 17:16:43 -0700

    Add Android Map to discovery
    Create a map under discovery tab by using MapView.
    
    Signed-off-by: TengChou Yang <threefure@gmail.com>

--- app/src/main/java/com/amk2/musicrunner/discover/DiscoverFragment.java
+++ app/src/main/java/com/amk2/musicrunner/discover/DiscoverFragment.java
@@ -1,6 +1,8 @@
 package com.amk2.musicrunner.discover;
 
 import com.amk2.musicrunner.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 
 import android.app.Fragment;
 import android.os.Bundle;
@@ -10,10 +12,16 @@ import android.view.ViewGroup;
 
 public class DiscoverFragment extends Fragment {
 
-	@Override
+    private MapView mapView;
+    private GoogleMap map;
+
+    @Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.discover_fragment, container, false);
+        View v = inflater.inflate(R.layout.discover_fragment, container, false);
+        mapView = (MapView) v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        return v;
 	}
 
 	@Override
@@ -21,6 +29,28 @@ public class DiscoverFragment extends Fragment {
 		super.onActivityCreated(savedInstanceState);
 		// This function is like onCreate() in activity.
 		// You can start from here.
+        map = mapView.getMap();
 	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 
 }
