32e3c4980389890ce06f82a6e504ef7a7846ed6d
logicmelody <logicmelody@gmail.com>
2014-06-30 21:56:40 +0800

    Style tab view in My page

--- app/src/main/java/com/amk2/musicrunner/my/MyFragment.java
+++ app/src/main/java/com/amk2/musicrunner/my/MyFragment.java
@@ -1,5 +1,6 @@
 package com.amk2.musicrunner.my;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -21,6 +22,7 @@ import android.widget.TextView;
 
 import com.amk2.musicrunner.Constant;
 import com.amk2.musicrunner.R;
+import com.amk2.musicrunner.RunningTabContentFactory;
 import com.amk2.musicrunner.sqliteDB.MusicTrackMetaData;
 import com.amk2.musicrunner.utilities.RestfulUtility;
 import com.amk2.musicrunner.utilities.SharedPreferencesUtility;
@@ -43,14 +45,25 @@ import java.util.List;
 
 public class MyFragment extends Fragment implements TabHost.OnTabChangeListener, View.OnClickListener {
 
+    public static final String RUNNING_TAB_TAG = "running_tab_tag";
+    public static final String MUSIC_TAB_TAG = "music_tab_tag";
+
+    public static class MyTabState {
+        public static final int RUNNING = 0;
+        public static final int MUSIC = 1;
+    }
+
     public interface MyTabFragmentListener {
         void onSwitchBetweenMyAndPastRecordFragment();
     }
+
+    private Activity mActivity;
 
     private ContentResolver mContentResolver;
 
     private MyTabFragmentListener mMyTabFragmentListener;
 
+    private TabHost mTabHost;
     private ProgressBar timesBar;
     private ProgressBar speedsBar;
     private ProgressBar caloriesBar;
@@ -75,48 +88,18 @@ public class MyFragment extends Fragment implements TabHost.OnTabChangeListener,
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+        mActivity = getActivity();
 		// This function is like onCreate() in activity.
 		// You can start from here.
         SharedPreferences preferences = this.getActivity().getSharedPreferences(Constant.PREFERENCE_NAME, Context.MODE_PRIVATE);
         String account = preferences.getString(Constant.ACCOUNT_PARAMS, null);
 //        TextView textView = (TextView) getView().findViewById(R.id.my_view);
 //        textView.setText(account,TextView.BufferType.EDITABLE);
-        initializeTab();
+        initialize();
 	}
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        setValueOfProgressBar();
-    }
-
-    private void initializeTab(){
-        TabHost tabHost = (TabHost) getView().findViewById(R.id.my_tab_host);
-        tabHost.setup();
-        TabHost.TabSpec spec = tabHost.newTabSpec("Running");
-        spec.setContent(R.id.my_running_tab);
-        spec.setIndicator("Running", getResources().getDrawable(android.R.drawable.ic_lock_idle_alarm));
-        tabHost.addTab(spec);
-        TabHost.TabSpec spec2 = tabHost.newTabSpec("Music");
-        spec2.setContent(R.id.my_music_tab);
-        spec2.setIndicator("Music", getResources().getDrawable(android.R.drawable.ic_lock_idle_alarm));
-        tabHost.addTab(spec2);
-
-        tabHost.setCurrentTab(0);
-
-        //set default tab background color
-        tabHost.getTabWidget().getChildAt(0).setBackgroundColor(Color.BLACK);
-        tabHost.getTabWidget().getChildAt(1).setBackgroundColor(Color.WHITE);
-        //set tab text color by selector
-        TabWidget tabWidget = (TabWidget) tabHost.findViewById(android.R.id.tabs);
-        View tabView = tabWidget.getChildTabViewAt(0);
-        TextView tab = (TextView) tabView.findViewById(android.R.id.title);
-        tab.setTextColor(this.getResources().getColorStateList(R.drawable.my_tab_selector));
-        View tabView2 = tabWidget.getChildTabViewAt(1);
-        TextView tab2 = (TextView) tabView2.findViewById(android.R.id.title);
-        tab2.setTextColor(this.getResources().getColorStateList(R.drawable.my_tab_selector));
-        //set tab background color depending on selected/unselected
-        tabHost.setOnTabChangedListener(this);
+    private void initialize() {
+        initTabs();
 
         mContentResolver = getActivity().getContentResolver();
 
@@ -139,6 +122,72 @@ public class MyFragment extends Fragment implements TabHost.OnTabChangeListener,
         String userName = SharedPreferencesUtility.getAccount(getActivity());
         TextView userNameTextView = (TextView) getView().findViewById(R.id.my_user_name);
         userNameTextView.setText(userName);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        setValueOfProgressBar();
+    }
+
+    private void initTabs(){
+        mTabHost = (TabHost) getView().findViewById(R.id.my_tab_host);
+        LayoutInflater layoutInflater = LayoutInflater.from(mActivity);
+        layoutInflater.inflate(R.layout.running_map_tab, null);
+
+        mTabHost.setup();
+        addTab(RUNNING_TAB_TAG, "Running");
+        addTab(MUSIC_TAB_TAG, "Music");
+
+//        TabHost.TabSpec spec = mTabHost.newTabSpec("Running");
+//        spec.setContent(R.id.my_running_tab);
+//        spec.setIndicator("Running", getResources().getDrawable(android.R.drawable.ic_lock_idle_alarm));
+//        mTabHost.addTab(spec);
+//        TabHost.TabSpec spec2 = mTabHost.newTabSpec("Music");
+//        spec2.setContent(R.id.my_music_tab);
+//        spec2.setIndicator("Music", getResources().getDrawable(android.R.drawable.ic_lock_idle_alarm));
+//        mTabHost.addTab(spec2);
+
+        mTabHost.setCurrentTab(0);
+
+//        //set default tab background color
+//        tabHost.getTabWidget().getChildAt(0).setBackgroundColor(Color.BLACK);
+//        tabHost.getTabWidget().getChildAt(1).setBackgroundColor(Color.WHITE);
+//        //set tab text color by selector
+//        TabWidget tabWidget = (TabWidget) tabHost.findViewById(android.R.id.tabs);
+//        View tabView = tabWidget.getChildTabViewAt(0);
+//        TextView tab = (TextView) tabView.findViewById(android.R.id.title);
+//        tab.setTextColor(this.getResources().getColorStateList(R.drawable.my_tab_selector));
+//        View tabView2 = tabWidget.getChildTabViewAt(1);
+//        TextView tab2 = (TextView) tabView2.findViewById(android.R.id.title);
+//        tab2.setTextColor(this.getResources().getColorStateList(R.drawable.my_tab_selector));
+        //set tab background color depending on selected/unselected
+        mTabHost.setOnTabChangedListener(this);
+    }
+
+    private void addTab(String tag, String labelText) {
+        View tabView = getTabView(tag);
+        TextView tabText = (TextView)tabView.findViewById(R.id.tab_text);
+        tabText.setText(labelText);
+        int contentId = -1;
+        if(RUNNING_TAB_TAG.equals(tag)) {
+            contentId = R.id.my_running_tab;
+        } else if(MUSIC_TAB_TAG.equals(tag)) {
+            contentId = R.id.my_music_tab;
+        }
+        mTabHost.addTab(mTabHost.newTabSpec(tag).setIndicator(tabView)
+                .setContent(contentId));
+    }
+
+    private View getTabView(String tag) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mActivity);
+        View tabView = new View(mActivity);
+        if(RUNNING_TAB_TAG.equals(tag)) {
+            tabView = layoutInflater.inflate(R.layout.my_running_tab, null);
+        } else if(MUSIC_TAB_TAG.equals(tag)) {
+            tabView = layoutInflater.inflate(R.layout.my_music_tab, null);
+        }
+        return tabView;
     }
 
     private String getMyStatus(){
@@ -202,14 +251,13 @@ Log.d("daz", "highestspeed" + highestSpeed.intValue() + " totalcalories" + total
 
     @Override
     public void onTabChanged(String tabId) {
-        TabHost tab = (TabHost) getView().findViewById(R.id.my_tab_host);
-        for (int i = 0; i < tab.getTabWidget().getChildCount(); i++) {
-            tab.getTabWidget().getChildAt(i)
-                    .setBackgroundResource(R.color.my_background_tab_unselected); // unselected
-        }
-        tab.getTabWidget().getChildAt(tab.getCurrentTab())
-                .setBackgroundResource(R.color.my_background_tab_selected); // selected
-
+//        TabHost tab = (TabHost) getView().findViewById(R.id.my_tab_host);
+//        for (int i = 0; i < tab.getTabWidget().getChildCount(); i++) {
+//            tab.getTabWidget().getChildAt(i)
+//                    .setBackgroundResource(R.color.my_background_tab_unselected); // unselected
+//        }
+//        tab.getTabWidget().getChildAt(tab.getCurrentTab())
+//                .setBackgroundResource(R.color.my_background_tab_selected); // selected
     }
 
     @Override
