96a79015ddf4d338b101a7998656c983cf36adff
Paul Ou <paulou1009@gmail.com>
2014-06-29 19:48:22 -0700

    setting page

--- app/src/main/java/com/amk2/musicrunner/setting/SettingFragment.java
+++ app/src/main/java/com/amk2/musicrunner/setting/SettingFragment.java
@@ -1,19 +1,19 @@
 package com.amk2.musicrunner.setting;
 
-import com.amk2.musicrunner.R;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amk2.musicrunner.R;
+
 public class SettingFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.discover_fragment, container, false);
+		return inflater.inflate(R.layout.setting_fragment, container, false);
 	}
 
 	@Override
