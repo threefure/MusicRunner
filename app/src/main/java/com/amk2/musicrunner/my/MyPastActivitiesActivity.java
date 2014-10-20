6b995ff2a48ea8e9f1207ed650df70a07de348bd
Daz <daz.lee1987@gmail.com>
2014-10-20 21:21:13 +0800

    [MUS-19] past activities list page can read units in setting page

--- app/src/main/java/com/amk2/musicrunner/my/MyPastActivitiesActivity.java
+++ app/src/main/java/com/amk2/musicrunner/my/MyPastActivitiesActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -16,13 +17,18 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.amk2.musicrunner.Constant;
 import com.amk2.musicrunner.R;
+import com.amk2.musicrunner.setting.SettingActivity;
 import com.amk2.musicrunner.sqliteDB.MusicRunnerDBMetaData.MusicRunnerRunningEventDB;
 import com.amk2.musicrunner.utilities.PhotoLib;
+import com.amk2.musicrunner.utilities.StringLib;
 import com.amk2.musicrunner.utilities.TimeConverter;
+import com.amk2.musicrunner.utilities.UnitConverter;
 
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.prefs.PreferenceChangeEvent;
 
 /**
  * Created by ktlee on 9/10/14.
@@ -37,6 +43,8 @@ public class MyPastActivitiesActivity extends Activity implements View.OnClickLi
 
     private LinearLayout myPastActivityContainer;
     private LayoutInflater inflater;
+    private SharedPreferences mSettingSharedPreferences;
+    private Integer distanceUnit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +58,8 @@ public class MyPastActivitiesActivity extends Activity implements View.OnClickLi
 
     private void initialize() {
         mActionBar = getActionBar();
+        mSettingSharedPreferences = getSharedPreferences(SettingActivity.SETTING_SHARED_PREFERENCE, 0);
+        distanceUnit = mSettingSharedPreferences.getInt(SettingActivity.DISTANCE_UNIT, SettingActivity.SETTING_DISTANCE_KM);
         initActionBar();
         initViews();
     }
@@ -84,15 +94,17 @@ public class MyPastActivitiesActivity extends Activity implements View.OnClickLi
         cursor.close();
     }
 
-    private void addPastActivity(Integer eventId, Integer duration, String distance, String currentEpoch, String photoPath) {
+    private void addPastActivity(Integer eventId, Integer duration, String distanceString, String currentEpoch, String photoPath) {
         View pastActivity = inflater.inflate(R.layout.my_past_activity_template, null);
-        ImageView photoImageView = (ImageView) pastActivity.findViewById(R.id.my_past_activity_photo);
+        ImageView photoImageView        = (ImageView) pastActivity.findViewById(R.id.my_past_activity_photo);
         ImageView dayConditionImageView = (ImageView) pastActivity.findViewById(R.id.my_past_activity_day_condition);
-        TextView dateTextView = (TextView) pastActivity.findViewById(R.id.my_past_activity_date);
-        TextView durationTextView = (TextView) pastActivity.findViewById(R.id.my_past_activity_duration);
-        TextView distanceTextView = (TextView) pastActivity.findViewById(R.id.my_past_activity_distance);
-        String dateString;
+        TextView dateTextView           = (TextView) pastActivity.findViewById(R.id.my_past_activity_date);
+        TextView durationTextView       = (TextView) pastActivity.findViewById(R.id.my_past_activity_duration);
+        TextView distanceTextView       = (TextView) pastActivity.findViewById(R.id.my_past_activity_distance);
+        TextView distanceUnitTextView   = (TextView) pastActivity.findViewById(R.id.my_past_activity_distance_unit);
+        String dateString, distanceUnitString;
         Integer dayPeriod, dayConditionId;
+        Double distance;
 
         Calendar date = Calendar.getInstance();
 
@@ -102,7 +114,17 @@ public class MyPastActivitiesActivity extends Activity implements View.OnClickLi
         }
 
         durationTextView.setText(TimeConverter.getDurationString(TimeConverter.getReadableTimeFormatFromSeconds(duration)));
-        distanceTextView.setText(distance);
+
+        // setting distance information
+        if (distanceUnit == SettingActivity.SETTING_DISTANCE_MI) {
+            distance = Double.parseDouble(distanceString);
+            distance = UnitConverter.getMIFromKM(distance);
+            distanceString = StringLib.truncateDoubleString(distance.toString(), 2);
+        }
+        distanceUnitString = Constant.DistanceMap.get(distanceUnit);
+        distanceTextView.setText(distanceString);
+        distanceUnitTextView.setText(distanceUnitString);
+
 
         date.setTimeInMillis(Long.parseLong(currentEpoch));
         dateString = (date.get(Calendar.MONTH) + 1) + "/" + date.get(Calendar.DAY_OF_MONTH) + " " + date.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
